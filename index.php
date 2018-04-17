25031db60ab9a33b58220ecff34877373fadef89
jconzy <34781857+jconzy@users.noreply.github.com>
2018-04-17 11:51:46 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<html doctype!>
+<html>
+	<head>
+        <?php 
+            date_default_timezone_set("Africa/Lagos");
+            $time = date("h:i A");
+        
+        ?>
+        
+		<title> HNG internship </title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/fontawesome.css">
+		<meta name="description" content="HNG internship.">
+		<meta charset= "UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="Content-Language" content="en-us"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+    <body>
+        <nav class="col-12 nav">
+            <div class="nav-bar col-10">
+                <ul>
+                    <a href="HNG.html" class="active">Home</a>
+                    <a href="HNG.html">Services</a>
+                    <a href="HNG.html">Contact</a>
+                </ul>
+            </div>
+        </nav>
+        
+        <div class=" col-12 container">
+            <p> BECOME A DEVELOPER </p>
+            <h2 class="time"> <?php  echo $time ?> </h2>
+        </div>
+    </body>
+    
+    
+</html>
